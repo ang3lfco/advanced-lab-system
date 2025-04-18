06e7d1ec9463a1bf2a432479202ad74947c59138
ang3lfco <ang3lfco@outlook.com>
2025-04-18 01:59:42 -0700

    initial project structure

---
+++ models/src/main/java/com/mycompany/models/Models.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.models;
+
+/**
+ *
+ * @author ang3lfco
+ */
+public class Models {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ presentation/src/main/java/com/mycompany/presentation/Presentation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.presentation;
+
+/**
+ *
+ * @author ang3lfco
+ */
+public class Presentation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
